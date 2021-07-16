156939675f82b3383686c19b2e1d179287abaf2e
Peter Schwarz <pschwarz@bitwise.io>
2021-07-16 15:52:21 -0500

    Add base traits for links
    This change adds a Sender and Receiver trait that cover the basic
    operations for a Link.  Given Rust's ownership nature, a Link has been
    decomposed into sending and receiving halves.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libaugrim/src/links/mod.rs
+++ libaugrim/src/links/mod.rs
@@ -11,3 +11,55 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+//! Defines a set of communication abstractions.
+//!
+//! The communication abstraction is defined as a link[^note-1] between two processes.  Messages are
+//! exchanged between processes over a link. These messages are unique, and each process is
+//! uniquely identified.
+//!
+//! Given the nature of ownership in Rust, a link is provided by both a [`Sender`] and a
+//! [`Receiver`][^note-2].
+//!
+//! [^note-1]: For a full explanation of links, see Cachin, Guerraoui, and Rodrigues, **Reliable and
+//! Secure Distributed Programming**, 2nd ed., 2.4.
+//!
+//! [^note-2]: Note that in **Reliable and Secure Distributed Programming**, the terminology is
+//! "deliver". See 2.4.1.
+
+use crate::error::InternalError;
+use crate::message::Message;
+use crate::process::Process;
+
+/// The sending half of a link
+pub trait Sender<P, M>
+where
+    P: Process,
+    M: Message,
+{
+    /// Send a message to a given process.
+    ///
+    /// # Errors
+    ///
+    /// An [`InternalError`] is returned if the underlying implementation encounters an error
+    /// during send.
+    fn send(to_process: &P, message: M) -> Result<(), InternalError>;
+}
+
+/// The receiving half of a link.
+///
+/// Concrete Receivers receive a message from a process, and may act on that message as they
+/// see fit. In other words, a message is delivered to the receiver.
+pub trait Receiver<P, M>
+where
+    P: Process,
+    M: Message,
+{
+    /// Receive a message from a process.
+    ///
+    /// # Errors
+    ///
+    /// An [`InternalError`] is returned if the underlying implementation encounters an error
+    /// while processing the received message.
+    fn recv(from_process: &P, message: M) -> Result<(), InternalError>;
+}
