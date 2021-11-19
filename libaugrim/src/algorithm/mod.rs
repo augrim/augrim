6688edaef7f014696d8f213ce5691d683b8518bb
Shawn T. Amundson <amundson@bitwise.io>
2021-11-19 10:08:30 -0600

    Add algorithm module and feature
    This module will contain all algorithm traits and implementations.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- libaugrim/src/algorithm/mod.rs
+++ libaugrim/src/algorithm/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2021 Cargill Incorporated
+// Copyright 2021-2022 Cargill Incorporated
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,14 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#[macro_use]
-extern crate log;
-
-#[cfg(feature = "algorithm")]
-pub mod algorithm;
-pub mod error;
-#[cfg(feature = "links")]
-pub mod links;
-pub mod message;
-pub mod process;
